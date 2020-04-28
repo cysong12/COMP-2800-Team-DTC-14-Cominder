2c82d7b4bb51b6e8d1fc91d5b1b1f0480b30974d
yeonseongjun <syeon1@my.bcit.ca>
2020-04-28 13:54:34 -0700

    added navigation bar, added static file to load static files, added css to make navbar look better

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='task-tracker-home'),
-	path('about/', views.about, name='blog-about'),
+	path('about/', views.about, name='task-tracker-about'),
 ]
