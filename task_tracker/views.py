2f4dc5c82d2dca2538e1370c1f1f260dd5b28169
yeonseongjun <syeon1@my.bcit.ca>
2020-04-27 17:45:53 -0700

    create a url directory for about page

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
 	return HttpResponse('<h1>Home page successfully rendered!!!!! WOOOOOOOOOOO</h1>')
+
+def about(request):
+	return HttpResponse('<h1>Does the About page render as well???? YESSSSSSSSSSSSSS</h1>')
