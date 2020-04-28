a9561a479d0c914fe4ff1baba6899c085d716da3
yeonseongjun <syeon1@my.bcit.ca>
2020-04-27 17:40:37 -0700

    Added a page for home page, tested if page rendered successfully

--- Cominder/settings.py
+++ Cominder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task_tracker.apps.TaskTrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
