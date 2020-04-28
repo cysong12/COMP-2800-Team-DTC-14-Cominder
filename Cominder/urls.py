0efcd76bfc3476d99a5662f04b924d7190a91317
yeonseongjun <syeon1@my.bcit.ca>
2020-04-27 17:56:42 -0700

    added templates for home and about subdirecteries

--- Cominder/urls.py
+++ Cominder/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task_tracker/', include('task_tracker.urls')),
+    path('', include('task_tracker.urls')),
 ]
--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-	return HttpResponse('<h1>Home page successfully rendered!!!!! WOOOOOOOOOOO</h1>')
+	return render(request, 'task_tracker/home.html')
 
 def about(request):
-	return HttpResponse('<h1>Does the About page render as well???? YESSSSSSSSSSSSSS</h1>')
+	return render(request, 'task_tracker/about.html')
