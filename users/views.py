6b52b72dac2d647a57ee17a845eb34a79de494e0
yeonseongjun <syeon1@my.bcit.ca>
2020-04-28 23:06:24 -0700

    implemented crispy forms so register form looks better

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, 'Your account has been created! You are now registered.')
             return redirect('task-tracker-home')
     else:
         form = UserRegisterForm()
