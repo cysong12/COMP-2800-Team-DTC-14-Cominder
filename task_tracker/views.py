438f7da4fe61279c2b7e28f4cb31bcd0f1b7cae9
yeonseongjun <syeon1@my.bcit.ca>
2020-04-28 22:58:21 -0700

    created a registration page and it WORKS

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Task
 
 def home(request):
-	return render(request, 'task_tracker/home.html')
+    context = {'tasks': Task.objects.all()
+	}
+    return render(request, 'task_tracker/home.html', context)
 
 def about(request):
-	return render(request, 'task_tracker/about.html')
+	return render(request, 'task_tracker/about.html', {'title': 'About us'})
