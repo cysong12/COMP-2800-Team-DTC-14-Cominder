a7998cc024c147c938a26e5b0b69a1c5081e22d3
yeonseongjun <syeon1@my.bcit.ca>
2020-04-28 21:28:40 -0700

    added a registration page

--- Cominder/settings.py
+++ Cominder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'task_tracker.apps.TaskTrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
