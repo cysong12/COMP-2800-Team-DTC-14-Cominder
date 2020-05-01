12c958b0c340792f84456e1d29c973dfcf7032b5
choi <cysong12@gmail.com>
2020-04-30 21:24:20 -0600

    base template

--- users/reminder_feature_temp.py
+++ users/reminder_feature_temp.py
@@ -9,13 +9,34 @@ To be implemented/moved in views.
 
 
 class Task:
-    def __init__(self, task_name, task_duration=None, task_detail=None):
-        self.__task_name = task_name
-        self.__task_detail = task_detail
+    def __init__(self, task_name: str, task_duration: datetime.datetime = None, date: datetime.datetime = None,
+                 task_detail: str = None, task_reward_point: int = None):
+        self.__name = task_name
+        self.__date = date     # datetime obj
+        self.__detail = task_detail        # user memo
         self.__duration = datetime.timedelta(days=1) if task_duration is None else \
             datetime.timedelta(weeks=task_duration.weeks, days=task_duration.days, hours=task_duration.hours,
                                minutes=task_duration.minutes)
         self.__completed = False
+        self.__task_reward_point = task_reward_point if None else 1
+
+    def get_name(self):
+        return self.__name
+
+    def get_date(self):
+        return self.__date
+
+    def get_detail(self):
+        return self.__detail
+
+    def get_duration(self):
+        return self.__duration
+
+    def is_completed(self):
+        return self.__completed
+
+    def get_point_weight(self):
+        return self.__task_reward_point
 
 
 class Tasks:
@@ -25,7 +46,16 @@ class Tasks:
     def add(self, task: Task):
         self.__tasks.append(task)       # sort input
 
+    def remove(self, task: Task):
+        self.__tasks.remove(task.get_name())
 
+    def clear(self):
+        self.__tasks = []
+
+    def size(self):
+        return len(self.__tasks)
+
+    
 class User:
     def __init__(self):
         self.__tasks = Tasks()
