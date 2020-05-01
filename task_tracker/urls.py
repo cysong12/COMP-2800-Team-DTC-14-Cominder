0b56dacf9e99cddc5a9e4bbd211faca160b8686a
Jessica Hong <jeessicahong@gmail.com>
2020-04-30 20:21:21 -0700

    Temporarily changed '/' directory to landing_page.html and finished adding CSS to the page

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='task-tracker-home'),
+	path('', views.landing_page, name='task-tracker-landing'),
+	path('home/', views.home, name='task-tracker-home'),
 	path('about/', views.about, name='task-tracker-about'),
 ]
