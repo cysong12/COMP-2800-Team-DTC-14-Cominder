b1e647df89d03b8009bc137673f5426bbc1607c5
choi <cysong12@gmail.com>
2020-05-07 14:41:10 -0600

    added more fields to task model

--- Apps/task_tracker/forms.py
+++ Apps/task_tracker/forms.py
@@ -6,10 +6,9 @@ from django.utils import timezone
 class TaskForm(forms.ModelForm):
     title = forms.CharField()
     description = forms.CharField()
-    # date_start = forms.DateTimeField()
-    # duration = forms.TimeField()
+    date_start = forms.DateTimeField()
+    duration = forms.TimeField()
 
     class Meta:
         model = Task
-        fields = ('title', 'description') #'date_posted')#, 'date_start', 'duration')
-        # fields = ('name', 'task_title', 'description', 'duration')
+        fields = ('title', 'description', 'date_start', 'duration')
--- Apps/task_tracker/models.py
+++ Apps/task_tracker/models.py
@@ -7,9 +7,9 @@ import datetime
 class Task(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True)
-    # date_posted = timezone.now()
-    # date_start = models.DateTimeField(default=timezone.now, null=True, blank=True)
-    # duration = models.DateTimeField(default=datetime.timedelta(hours=24), null=True, blank=True)
+    date_start = models.DateTimeField(default=timezone.now)
+    duration = models.TimeField(default=timezone.now)
+    date_posted = timezone.now()
     # tasks = models.ForeignKey('users.Tasks', on_delete=models.CASCADE)  # maybe not on delete? since users can have common tasks
     
     def __str__(self):
