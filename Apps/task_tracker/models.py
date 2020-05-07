e3120340349e1b07dbff1136af64a1b8c6b655c6
choi <cysong12@gmail.com>
2020-05-07 13:38:44 -0600

    migration success and render success for task

--- Apps/task_tracker/models.py
+++ Apps/task_tracker/models.py
@@ -5,12 +5,12 @@ import datetime
 
 
 class Task(models.Model):
-    task_title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True)
-    date_posted = timezone.now()
-    date_start = models.DateTimeField(default=timezone.now)
-    duration = models.DateTimeField(default=datetime.timedelta(hours=24))
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # date_posted = timezone.now()
+    # date_start = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    # duration = models.DateTimeField(default=datetime.timedelta(hours=24), null=True, blank=True)
+    # tasks = models.ForeignKey('users.Tasks', on_delete=models.CASCADE)  # maybe not on delete? since users can have common tasks
     
     def __str__(self):
-        return self.task_title
+        return self.title
