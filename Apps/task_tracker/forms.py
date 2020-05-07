31eb8119648ee6a46e150e8a8411002b96aaf201
choi <cysong12@gmail.com>
2020-05-06 18:27:38 -0600

    restructured paths

---
+++ Apps/task_tracker/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from task_tracker.models import Task
+from django.utils import timezone
+
+
+class TaskForm(forms.ModelForm):
+    name = forms.CharField()
+    task_title = forms.CharField()
+    description = forms.CharField()
+    date_start = forms.DateTimeField(default=timezone.now())
+    duration = forms.TimeField()
+
+    class Meta:
+        model = Task
+        fields = ('name', 'task_title', 'description', 'date_start', 'duration')
---
+++ Apps/task_tracker/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Task
+from django.contrib.auth.decorators import login_required
+from .forms import TaskForm
+
+
+@login_required
+def home(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+    else:
+        form = TaskForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'task_tracker/home.html', context)
+
+
+def about(request):
+    return render(request, 'task_tracker/about.html', {'title': 'About us'})
+
+
+def landing_page(request):
+    return render(request, 'task_tracker/landing_page.html')
+
+
+def contact(request):
+    return render(request, 'task_tracker/contact.html')
