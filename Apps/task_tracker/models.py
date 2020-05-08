280552a009a5ff6f4aa49bf4ff1c34850586e994
choi <cysong12@gmail.com>
2020-05-07 20:31:16 -0600

    db finally worksfor task!

--- Apps/task_tracker/models.py
+++ Apps/task_tracker/models.py
@@ -4,20 +4,14 @@ from django.contrib.auth.models import User
 import datetime
 
 
-class Tasks(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=None)
-
-    def __str__(self):
-        return f"{User.username} Tasks"
-
-
 class Task(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True)
     date_start = models.DateTimeField(default=timezone.now)
     duration = models.TimeField(default=timezone.now)
     date_posted = timezone.now()
-    tasks = models.ForeignKey(to=Tasks, on_delete=models.CASCADE, default=None)  # maybe not on delete? since users can have common tasks
-    
+    username = User.username
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
     def __str__(self):
         return f"{self.title}"
--- Apps/task_tracker/views.py
+++ Apps/task_tracker/views.py
@@ -1,27 +1,32 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from .models import Task, Tasks
+from django.views.generic import ListView
+from .models import Task
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
+from django.contrib.auth.models import User
 
 
 @login_required
 def home(request):
     task = ''
     if request.method == "POST":
-        form = TaskForm(request.POST or None, instance=Tasks)
+        form = TaskForm(request.POST or None)
         if form.is_valid():
             task = form.save()
         else:
             messages.error(request, "Please correct the errors marked in red.")
     else:
         form = TaskForm()
-
+    try:
+        tasks = Task.objects.filter(username='admin')
+    except Exception as e:
+        print(e)
     context = {
         'form': form,
         'task': task,
-        'tasks': Tasks.objects.all()
+        # 'tasks': tasks,
     }
 
     return render(request, 'task_tracker/home.html', context)
