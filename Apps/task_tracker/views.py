6793963638aa60ee5345559e5b76b027c76a9154
yeonseongjun <syeon1@my.bcit.ca>
2020-05-08 03:44:17 -0700

    Added create, update, delete features for tasks

--- Apps/task_tracker/views.py
+++ Apps/task_tracker/views.py
@@ -1,37 +1,61 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
 from django.contrib.auth.decorators import login_required
-from .forms import TaskForm
-from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 
 @login_required
 def home(request):
-    task = ''
-    if request.method == "POST":
-        form = TaskForm(request.POST or None)
-        if form.is_valid():
-            task = form.save()
-        else:
-            messages.error(request, "Please correct the errors marked in red.")
-    else:
-        form = TaskForm()
-    try:
-        tasks = Task.objects.filter(username='admin')
-    except Exception as e:
-        print(e)
     context = {
-        'form': form,
-        'task': task,
-        # 'tasks': tasks,
+        'tasks': Task.objects.filter(username=request.user.username)
     }
-
     return render(request, 'task_tracker/home.html', context)
 
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_tracker/home.html'
+    context_object_name = 'tasks'
+    ordering = ['-date_posted']
 
+class TaskDetailView(DetailView):
+    model = Task
+    
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    success_url = '/task-tracker/home'
+    fields = ['title', 'description']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.user:
+            return True
+        return False
+    
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Task
+    success_url = '/task-tracker/home'
+    
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.user:
+            return True
+        return False
+    
+    
 def about(request):
     return render(request, 'task_tracker/about.html', {'title': 'About us'})
 
