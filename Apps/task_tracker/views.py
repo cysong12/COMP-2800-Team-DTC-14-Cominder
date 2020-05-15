7131abba025a5fb0d31ca6a5df23ed75bc7617e0
dkeen10 <duncankeen@gmail.com>
2020-05-14 17:10:05 -0700

    added send_push to task_tracker/views

--- Apps/task_tracker/views.py
+++ Apps/task_tracker/views.py
@@ -8,6 +8,17 @@ from .forms import *
 from django.http import HttpResponseRedirect
 from django.db.models import Q
 
+from django.http.response import JsonResponse, HttpResponse
+from django.views.decorators.http import require_GET, require_POST
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from webpush import send_user_notification
+import json
+
+import datetime
+import sched
+import time
+
 
 @login_required
 def home(request):
@@ -101,3 +112,26 @@ def landing_page(request):
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+
+@require_POST
+@csrf_exempt
+def send_push(request):
+    try:
+        body = request.body
+        data = json.loads(body)
+
+        if 'head' not in data or 'body' not in data or 'id' not in data:
+            return JsonResponse(status=400, data={"message": "Invalid data format"})
+
+        user = get_object_or_404(Task.user, pk=1)
+        payload = {'head': Task.title, 'body': Task.description}
+        push_notification = send_user_notification(user=user, payload=payload, ttl=1000)
+
+        schedule = sched.scheduler(time.localtime, time.sleep)
+        schedule.enterabs(time.strptime(Task.start_time), 0, push_notification)
+        schedule.run()
+        return JsonResponse(status=200, data={"message": "Web push successful"})
+
+    except TypeError:
+        return JsonResponse(status=500, data={"message": "An error occurred"})
