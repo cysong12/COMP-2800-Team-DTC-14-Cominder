fe3de08484208fc75b07fc7ee5edca1c320254ee
Jessica Hong <jeessicahong@gmail.com>
2020-05-14 22:34:33 -0700

    Created Fridge models and views

--- Apps/fridge/models.py
+++ Apps/fridge/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Fridge(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('fridge-detail', kwargs={'pk': self.pk})
+
+
+class Recipe(models.Model):
+    pass
--- Apps/fridge/views.py
+++ Apps/fridge/views.py
@@ -1,3 +1,71 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import *
 
-# Create your views here.
+# Create your class-based views here.
+
+
+@login_required
+def home(request):
+    fridge_items = Fridge.objects.all()
+    context = {
+        'fridge_items': fridge_items
+    }
+
+    return render(request, 'fridge/home.html', context)
+
+
+class FridgeListView(ListView):
+    # Tells what model to query
+    model = Fridge
+    # Template
+    template_name = 'fridge/home.html'
+
+
+class FridgeDetailView(DetailView):
+    model = Fridge
+
+
+# CREATE ITEM
+class FridgeCreateView(LoginRequiredMixin, CreateView):
+    model = Fridge
+    fields = ['name', 'quantity']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# UPDATE ITEM
+class FridgeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Fridge
+    # When successfully deleted, will take user back to homepage
+    success_url = '/'
+    fields = ['name', 'quantity']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        fridge_item = self.get_object()
+        # Prevents others to update other people's items
+        if self.request.user == fridge_item.user:
+            return True
+        return False
+
+
+class FridgeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Fridge
+    # When successfully deleted, will take user back to homepage
+    success_url = '/'
+
+    def test_func(self):
+        fridge_item = self.get_object()
+        # Prevents others to update other people's items
+        if self.request.user == fridge_item.user:
+            return True
+        return False
+
