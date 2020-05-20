c37c015620f3a065d7fa305123aca72811202713
yeonseongjun <syeon1@my.bcit.ca>
2020-05-19 20:28:58 -0700

    added password reset

--- Cominder/settings.py
+++ Cominder/settings.py
@@ -154,4 +154,19 @@ MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'noreply.cominder@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'sgnbkeycznxuvhjk'
+
+DEFAULT_FROM_EMAIL = 'noreply.cominder@gmail.com'
+
+
 
